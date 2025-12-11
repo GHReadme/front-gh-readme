530e9ea99a54d1145feba6d0b1b79a34415ccd2e
Дмитрий <51026661+drozdovdn@users.noreply.github.com>
2025-12-12 01:04:41 +0300

    Fix color scheme manager usage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import React, { ReactNode } from 'react';
 
-import { ColorSchemeScript, MantineProvider, localStorageColorSchemeManager } from '@mantine/core';
-import { Notifications } from '@mantine/notifications';
+import { ColorSchemeScript } from '@mantine/core';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 
@@ -11,11 +10,7 @@ import './globals.css';
 import '@mantine/core/styles.css';
 import '@mantine/notifications/styles.css';
 
-import ThemeToggle from 'shared/ui/theme-toggle';
-
-import style from './layout.module.scss';
-
-const colorSchemeManager = localStorageColorSchemeManager({ key: 'gh-readme-color-scheme' });
+import { AppProviders } from './AppProviders';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -45,14 +40,7 @@ export default function RootLayout({
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen`} suppressHydrationWarning>
         <ReactQueryProvider>
-          <MantineProvider defaultColorScheme="auto" colorSchemeManager={colorSchemeManager}>
-            <Notifications />
-            <header className={style.header}>
-              <h1 className={style.title}>GH Readme</h1>
-              <ThemeToggle />
-            </header>
-            <div className="h-full w-full p-2">{children}</div>
-          </MantineProvider>
+          <AppProviders>{children}</AppProviders>
         </ReactQueryProvider>
       </body>
     </html>
